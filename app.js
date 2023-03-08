bbcaf170ed2d3f2abcca3bbc22643a6133f0f90c
Sophie <s.ischenko1990@gmail.com>
2023-03-08 14:01:37 +0100

    add ejs view

--- app.js
+++ app.js
@@ -5,9 +5,41 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+let today = new Date();
+let currentDay = today.getDay();
+let day = "";
+
 app.get("/", function(req, res) {
-    res.send("Hello");
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: current day is equal to: " + currentDay);
+    }
+    res.render("list", { kindOfDay: day })
 });
+
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
