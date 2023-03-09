7b1e6164e2200328c3e3c89651a3d049498ab239
sophie <s.ischenko1990@gmail.com>
2023-03-09 11:09:17 +0100

    finish To do list with different sites

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-let ToDos= ["Buy Food", "Cook the Food", "Eat the Food"]
+let ToDos= ["Buy Food", "Cook the Food", "Eat the Food"];
+let workToDos = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     let today = new Date();
@@ -20,16 +22,36 @@ app.get("/", (req, res) => {
     let day= today.toLocaleDateString("en-US", options)
 
     res.render("list", { 
-        kindOfDay: day,
+        listTitle: day,
         newListToDos : ToDos })
 });
 
 app.post("/", (req, res)=>{
     let nextToDo = req.body.nextToDo;
-    ToDos.push(nextToDo);
-    res.redirect("/");
+    
+    if(req.body.list==='Work'){
+        workToDos.push(nextToDo);
+        res.redirect("/work");
+    }
+    else{
+        ToDos.push(nextToDo);
+        res.redirect("/");
+    }
 })
 
+app.get("/work", (req, res)=>{
+    res.render("list", {listTitle:"Work List", newListToDos: workToDos})
+})
+
+app.post("/work", (req, res)=>{
+    let nextToDo = req.body.nextToDo;
+    workToDos.push(nextToDo);
+    res.redirect("/work");
+})
+
+app.get("/about", (req, res)=>{
+    res.render("about");
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
