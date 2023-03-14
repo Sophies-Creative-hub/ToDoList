d7c940f2bfd9351d7cb2ba14403a19f3d21bfb39
Sophie <s.ischenko1990@gmail.com>
2023-03-14 12:32:14 +0100

    todos in new db

--- app.js
+++ app.js
@@ -1,52 +1,75 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
-const date = require(__dirname+"/date.js")
-
-const ToDos= ["Buy Food", "Cook the Food", "Eat the Food"];
-const workToDos = [];
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
+
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = ({
+    name: String
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems)
+.then(() => {
+
+    console.log('successfully added all to todo db');
+})   .catch((error) => {
+        console.log(error);
+    });
 
 app.get("/", (req, res) => {
     let day = date.getDay()
-    res.render("list", { 
-        listTitle: day,
-        newListToDos : ToDos })
+    res.render("list", {
+        listTitle: 'Today',
+        newListToDos: ToDos
+    })
 });
 
-app.post("/", (req, res)=>{
+app.post("/", (req, res) => {
     let nextToDo = req.body.nextToDo;
-    
-    if(req.body.list==='Work'){
+
+    if (req.body.list === 'Work') {
         workToDos.push(nextToDo);
         res.redirect("/work");
     }
-    else{
+    else {
         ToDos.push(nextToDo);
         res.redirect("/");
     }
 })
 
-app.get("/work", (req, res)=>{
-    res.render("list", {listTitle:"Work List", newListToDos: workToDos})
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work List", newListToDos: workToDos })
 })
 
-app.post("/work", (req, res)=>{
+app.post("/work", (req, res) => {
     let nextToDo = req.body.nextToDo;
     workToDos.push(nextToDo);
     res.redirect("/work");
 })
 
-app.get("/about", (req, res)=>{
+app.get("/about", (req, res) => {
     res.render("about");
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Server started on port 3000.");
 });
