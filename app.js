76946972e96b01d0cbb93b03ff4a4abacd16a132
Sophie <s.ischenko1990@gmail.com>
2023-03-16 12:29:04 +0100

    custom lists

--- app.js
+++ app.js
@@ -27,44 +27,88 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems)
-.then(() => {
-
-    console.log('successfully added all to todo db');
-})   .catch((error) => {
-        console.log(error);
-    });
+const listSchema = ({
+    name: String,
+    items: [itemsSchema]
+});
+const List = mongoose.model("List", listSchema);
 
 app.get("/", (req, res) => {
-    let day = date.getDay()
-    res.render("list", {
-        listTitle: 'Today',
-        newListToDos: ToDos
-    })
+    Item.find({})
+        .exec()
+        .then(foundItems => {
+            console.log(foundItems);
+            if(foundItems.length === 0){
+                Item.insertMany(defaultItems)
+                .then(() => {
+                    console.log('successfully added all to todo db');
+                    res.redirect('/');
+                })   .catch((error) => {
+                        console.log(error);
+                    });
+            }
+            res.render("list", {
+                listTitle: 'Today',
+                newListToDos: foundItems
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
+
 app.post("/", (req, res) => {
-    let nextToDo = req.body.nextToDo;
-
-    if (req.body.list === 'Work') {
-        workToDos.push(nextToDo);
-        res.redirect("/work");
-    }
-    else {
-        ToDos.push(nextToDo);
-        res.redirect("/");
-    }
+    let nextToDoName = req.body.nextToDo;
+    let nextToDo = new Item({
+        name: nextToDoName
+    });
+    nextToDo.save();
+    res.redirect("/");
 })
 
-app.get("/work", (req, res) => {
-    res.render("list", { listTitle: "Work List", newListToDos: workToDos })
-})
+app.post("/delete", (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId)
+      .then(() => {
+        console.log('successfully deleted item');
+        res.redirect('/');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+  
 
-app.post("/work", (req, res) => {
-    let nextToDo = req.body.nextToDo;
-    workToDos.push(nextToDo);
-    res.redirect("/work");
-})
+  app.get("/:categoryName", (req, res) => {
+    const categoryName = req.params.categoryName;
+
+    List.findOne({name: categoryName})
+        .then(foundList => {
+            if(!foundList) {
+                // Wenn keine Liste mit dem Namen gefunden wurde, erstelle eine neue Liste
+                const list = new List({
+                    name: categoryName,
+                    items: defaultItems
+                });
+                return list.save();
+            } else {
+                // Wenn eine Liste mit dem Namen gefunden wurde, zeige die Liste an
+                return foundList;
+            }
+        })
+        .then(list => {
+            res.render("list", {
+                listTitle: list.name,
+                newListToDos: list.items
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
 
 app.get("/about", (req, res) => {
     res.render("about");
@@ -72,4 +116,4 @@ app.get("/about", (req, res) => {
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
-});
+})
